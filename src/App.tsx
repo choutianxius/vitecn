4783789a60c25e556934eca99ec6a71fe6177d6f
choutianxius <choutianxius@gmail.com>
2024-09-06 17:50:33 -0500

    Use pnpm; fix typescript alias

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen flex">
+      <p className="m-auto">Modify Me</p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "@/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="w-screen h-screen flex">
-      <p className="m-auto">Modify Me</p>
-    </div>
+    <App />
   </React.StrictMode>
 );
